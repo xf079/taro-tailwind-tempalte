8d6d7229deb09a25de4c94be67f473b97df002ef
xfo79k <xfo79k@gmail.com>
2024-09-28 21:14:57 +0800

    feat: config

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -7,26 +7,30 @@ import { cva, VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center w-auto px-2 py-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring bg-transparent after:border-none',
+  'inline-flex items-center justify-center w-auto px-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring bg-transparent after:border-none',
   {
     variants: {
       variant: {
         default:
-          'bg-primary !text-primary-foreground shadow hover:bg-primary/90 active:!bg-primary/70',
+          'bg-primary !text-primary-foreground shadow active:!bg-primary/70',
         destructive:
-          'bg-destructive !text-destructive-foreground shadow-sm hover:bg-destructive/90 active:!bg-destructive/70',
+          'bg-destructive !text-destructive-foreground shadow-sm active:!bg-destructive/70',
         outline:
-          'border-2 border-input border-solid bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
+          'border-2 border-input border-solid bg-background shadow-sm active:!bg-accent active:!text-accent-foreground',
         secondary:
-          'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 underline'
+          'bg-secondary text-secondary-foreground shadow-sm active:bg-secondary/80',
+        ghost: 'active:!bg-accent active:!text-accent-foreground',
+        link: 'text-primary underline-offset-4 underline active:!bg-transparent active:!text-primary/70'
       },
       size: {
-        default: 'h-9 px-4 py-2',
+        default: 'h-12 px-4 py-2',
         sm: 'h-8 rounded-md px-3 text-xs',
         lg: 'h-10 rounded-md px-8',
         icon: 'h-9 w-9'
+      },
+      shape: {
+        default: 'rounded-md',
+        circle: 'rounded-full'
       },
       block: {
         true: 'w-full'
@@ -38,6 +42,7 @@ const buttonVariants = cva(
     defaultVariants: {
       variant: 'default',
       size: 'default',
+      shape: 'default',
       block: false,
       disabled: false
     }
@@ -60,7 +65,6 @@ export const Button: FC<ButtonProps> = ({
       disabled={disabled}
       className={cn(
         buttonVariants({ variant, size, block, disabled, className }),
-        'cursor-pointer active:bg-primary/90'
       )}
       {...props}
     >
